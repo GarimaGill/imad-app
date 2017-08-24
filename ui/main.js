829078be3f20c7fb56a390635eca64f1caa8fceb
GarimaGill <31007104+GarimaGill@users.noreply.github.com>
2017-08-24 19:02:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,10 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 
 element.innerHTML='new value';
+
+//move image
+
+var madi= document.getElementById('madi');
+madi.onclick=function(){
+    madi.style.marginleft='100px';
+};
