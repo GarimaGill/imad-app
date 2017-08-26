b486f21cb74ddf9dc7916a3b327c2ecad07ef9a6
GarimaGill <31007104+GarimaGill@users.noreply.github.com>
2017-08-26 20:11:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.get('/', function (req, res) {
 var counter=0;
 app.get('/counter',function(req,res) {
    counter=counter+1;
-    res.send(counter.toString())
+    res.send(counter.toString());
 });
 var names=[];
 app.get('/submit-name',function(req,res){// /submit-name?name=xxxx
